79a2634fdc6432d4602b084e33852bddb319a999
sadrack404 <gabrielscr2023@gmail.com>
2023-01-17 16:08:27 -0300

    Inicio do projeto

---
+++ src/main/java/com/Attornatus/gerenciarPessoasapi/GerenciarPessoasApiApplication.java
@@ -0,0 +1,13 @@
+package com.Attornatus.gerenciarPessoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarPessoasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarPessoasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Attornatus/gerenciarPessoasapi/GerenciarPessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Attornatus.gerenciarPessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarPessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
